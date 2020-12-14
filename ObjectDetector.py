40b9ecf3e587aacb6a40085ce4b1290e6d53b8e7
Eric <ericj.marcus@gmail.com>
2020-12-15 00:06:27 +0100

    implemented bounding box drawing

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -6,23 +6,31 @@ from FeedForwardModel import FFM
 def main():
     """This is where the magic happens"""
     #  TODO: implement custom keras loss function by IOU
+    #   Also perhaps check if there are different results with pytorch or sklearn
+    #   in terms of the learning performance
 
     shape_creator = DataCreation.ShapeCreator()
     test_box = shape_creator.box_creator(flatten=False)
 
     # Utils.binary_array_plot(test_box[0])
-    train_x, train_y = shape_creator.box_dataset_creator(60000, 'box_data.csv')
+    train_x, train_y = shape_creator.box_dataset_creator(100000, 'box_data.csv')
     test_x, test_y = shape_creator.box_dataset_creator(10000, 'test_box_set.csv')
     # tdat = shape_creator.box_dataset_loader('box_data.csv')
 
     n = FFM()
     n.initialize_model()
-    n.train_model(train_x, train_y, 'objectdetect', batch_size=1000, epochs=100)
+    n.train_model(train_x, train_y, 'objectdetect', batch_size=1000, epochs=10)
     n.test_model(test_x, test_y)
 
-    print(test_box[0])
-    pred = n.prediction(test_box[0].flatten())
-    print(pred, test_box[1])
+    # print(test_box[0])
+    # pred = n.prediction(test_box[0].flatten())
+    # print(pred, test_box[1])
+    # Utils.binary_array_plot(test_box[0])
+    # Utils.draw_rectangle(test_box[1])
+    # Utils.draw_rectangle(pred, colour='b')
+    preds = n.prediction(test_x[0:10])
+    # TODO: fix the draw_predictions or change input to non flattened
+    Utils.draw_predictions(test_x[0:10], test_y[0:10], preds)
     print('hallo')
 
 
--- Utils.py
+++ Utils.py
@@ -1,10 +1,33 @@
+from Defines import background_size
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 # This function creates a plot of a np.array
 def binary_array_plot(array):
-    plt.imshow(array, cmap='binary')
-    plt.show()
+    plt.imshow(array, cmap='Greys', interpolation='none', origin='lower', extent=[0, background_size, 0, background_size])
+    plt.show(block=False)
+
+
+# This function draws a rectangle on inputted coordinates
+def draw_rectangle(coords, colour='r'):
+    coords = background_size * coords.flatten()
+    rect = patches.Rectangle((coords[0], coords[1]), coords[2], coords[3], linewidth=1, edgecolor=colour, facecolor='none')
+    plt.gca().add_patch(rect)
+    plt.show(block=False)
+
+
+# This function plots the prediction and true labels through inputted data
+def draw_predictions(input_boxes, input_labels, predicted_labels):
+    for i in range(len(input_boxes)):
+        binary_array_plot(input_boxes[i])
+        draw_rectangle(input_labels[i])
+        draw_rectangle(predicted_labels[i], colour='b')
+        inp = input("Press enter for continue or q for quit")
+        if inp == 'q':
+            return
+
+    return
 
 
 # This function splits the array at split_loc
